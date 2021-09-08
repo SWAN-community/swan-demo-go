71e3e2765cd146386cac5e6ade05ba65c2e50015
James Rosewell <james@51degrees.com>
2021-09-08 19:59:58 +0100

    CLEANUP: Avoided including owid.js twice in the demo.

--- www/demo.js
+++ www/demo.js
@@ -3,81 +3,75 @@
 var loaded = false;
 
 stop = function(s, d, r, a) {
-    loadOWID().then(() => {
-        var data = new URLSearchParams();
-        data.append("host", d);
-        data.append("returnUrl", r);
-        data.append("accessNode", s);
-        fetch("/stop?",
-            { 
-                method: "POST", 
-                mode: "cors", 
-                cache: "no-cache",
-                body: data 
-            })
-            .then(r => r.text() )
-            .then(m => {
-                console.log(m);
-                window.location.href = m;
-            })
-            .catch(x => {
-                console.log(x);
-            });
-    });
+    var data = new URLSearchParams();
+    data.append("host", d);
+    data.append("returnUrl", r);
+    data.append("accessNode", s);
+    fetch("/stop?",
+        { 
+            method: "POST", 
+            mode: "cors", 
+            cache: "no-cache",
+            body: data 
+        })
+        .then(r => r.text() )
+        .then(m => {
+            console.log(m);
+            window.location.href = m;
+        })
+        .catch(x => {
+            console.log(x);
+        });
 }
 
 appendComplaintEmail = function(e, d, o, s, g) {
-    loadOWID().then(() => {
-        var data = new URLSearchParams();
-        data.append("swanid", o);
-        data.append("partyid", s);
-        fetch((d ? "//" + d : "") + "/complain?",
-            { 
-                method: "POST", 
-                mode: "cors", 
-                cache: "no-cache",
-                body: data 
-            })
-            .then(r => r.text() )
-            .then(m => {
-                var a = document.createElement("a");
-                a.href = m;
-                if (g) {
-                    var i = document.createElement("img");
-                    i.src = g;
-                    i.style="width:32px"
-                    a.appendChild(i);
-                } else {
-                    a.innerText = "?";
-                }
-                e.appendChild(a);
-            }).catch(x => {
-                console.log(x);
-            });
-    });
+    var data = new URLSearchParams();
+    data.append("swanid", o);
+    data.append("partyid", s);
+    fetch((d ? "//" + d : "") + "/complain?",
+        { 
+            method: "POST", 
+            mode: "cors", 
+            cache: "no-cache",
+            body: data 
+        })
+        .then(r => r.text() )
+        .then(m => {
+            var a = document.createElement("a");
+            a.href = m;
+            if (g) {
+                var i = document.createElement("img");
+                i.src = g;
+                i.style="width:32px"
+                a.appendChild(i);
+            } else {
+                a.innerText = "?";
+            }
+            e.appendChild(a);
+        }).catch(x => {
+            console.log(x);
+        });
 }
 
 appendName = function(e, s) {
-    loadOWID().then(() => {
-        var supplier = new owid(s);
-        var url = "//" + supplier.domain + "/owid/api/v1/creator";
-        fetch(url, 
-            { method: "GET", mode: "cors" })
-            .then(r => {
-                if (r.ok) {
-                    return r.json();
-                }
-                throw "Bad response from '" + url + "'";
-            })
-            .then(o => {
-                var t = document.createTextNode(o.name);
-                e.appendChild(t);
-            }).catch(x => {
-                console.log(x);
-                var t = document.createTextNode(x);
-                e.appendChild(t);
-            });
-    });
+    var supplier = new owid(s);
+    var url = "//" + supplier.domain + "/owid/api/v1/creator";
+    fetch(url, 
+        { method: "GET", mode: "cors" })
+        .then(r => {
+            if (r.ok) {
+                return r.json();
+            }
+            throw "Bad response from '" + url + "'";
+        })
+        .then(o => {
+            var t = document.createTextNode(o.name);
+            e.appendChild(t);
+        }).catch(x => {
+            console.log(x);
+            var t = document.createTextNode(x);
+            e.appendChild(t);
+        });
 }
 
 appendAuditMark = function(e, r, t) {
@@ -114,27 +108,10 @@ appendAuditMark = function(e, r, t) {
     // supported the well known end point for OWID creators. OWID providers
     // are not required to operate an end point for verifying OWIDs so these
     // calls might fail to return a result.
-    loadOWID().then(() => {
-        verify(r, t)
-            .then(r => addAuditMark(e, r))
-            .catch(x => {
-                console.log(x);
-                returnFailed(e);
-            });
-    });
-}
-
-loadOWID = function() {
-    return new Promise((resolve, reject) => {
-        if(loaded || typeof owid !== "undefined") {
-            resolve();
-        }
-        var script = document.createElement('script');
-        script.onload = function () {
-            loaded = true;
-            resolve();
-        };
-        script.src = "/owid-js/v1.js";
-        document.head.appendChild(script);
-    });
+    verify(r, t)
+        .then(r => addAuditMark(e, r))
+        .catch(x => {
+            console.log(x);
+            returnFailed(e);
+        });
 }
